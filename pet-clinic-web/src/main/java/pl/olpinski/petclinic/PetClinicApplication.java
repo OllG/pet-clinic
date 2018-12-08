c57195b76ae9808110da9a7cee6edc7293009f7b
olg <olgierd@olpinski.pl>
2018-12-08 08:59:57 +0100

    Refactored AbstractMapService to generate ID. Closes #23

--- pet-clinic-web/src/main/java/pl/olpinski/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/pl/olpinski/petclinic/PetClinicApplication.java
@@ -3,10 +3,15 @@ package pl.olpinski.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class PetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
+
+        Set<Long> longs = new HashSet<>();
     }
 }
--- pet-clinic-web/src/main/java/pl/olpinski/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/olpinski/petclinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Jackson");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Jeffrey");
         owner2.setLastName("Williams");
         ownerService.save(owner2);
@@ -36,13 +34,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("John");
         vet1.setLastName("Thompson");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Mike");
         vet2.setLastName("Tyson");
         vetService.save(vet2);
